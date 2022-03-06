fbee2ff701ec3f9a50d600496cdb7d9a31001114
Iuterian99 <Abduhalim.orziqulov@bk.ru>
2022-03-06 17:48:35 +0500

    FINISHED SQL TABLES

--- src/model/fakedata.sql
+++ src/model/fakedata.sql
@@ -2,46 +2,46 @@ INSERT INTO building_companies(company_name) VALUES('Murad Buildings');
 INSERT INTO building_companies(company_name) VALUES('Golden House');
 INSERT INTO building_companies(company_name) VALUES('Akay City');
 
-INSERT INTO murad_buildings_NestOne(house_name, house_price, company_id) 
-VALUES('1-10 ETAJLAR', 1500, 'c0d6494d-779f-44cf-8eda-744778b87e3b'),
-      ('11-20 ETAJLAR', 1400, 'c0d6494d-779f-44cf-8eda-744778b87e3b'),
-      ('21-30 ETAJLAR', 1300, 'c0d6494d-779f-44cf-8eda-744778b87e3b'),
-      ('31-40 ETAJLAR', 1200, 'c0d6494d-779f-44cf-8eda-744778b87e3b'),
-      ('41-52 ETAJLAR', 1100, 'c0d6494d-779f-44cf-8eda-744778b87e3b');
+INSERT INTO NestOne(house_name, house_price, company_id) 
+VALUES('1-10 ETAJLAR', 1500, '268dce62-8937-42cc-ab51-46d5bb2b530c'),
+      ('11-20 ETAJLAR', 1400, '268dce62-8937-42cc-ab51-46d5bb2b530c'),
+      ('21-30 ETAJLAR', 1300, '268dce62-8937-42cc-ab51-46d5bb2b530c'),
+      ('31-40 ETAJLAR', 1200, '268dce62-8937-42cc-ab51-46d5bb2b530c'),
+      ('41-52 ETAJLAR', 1100, '268dce62-8937-42cc-ab51-46d5bb2b530c');
 
 INSERT INTO murad_buildings_millenium(house_name, house_price, company_id) 
-VALUES('1-2 ETAJLAR', 1200, 'c0d6494d-779f-44cf-8eda-744778b87e3b'),
-      ('3-5 ETAJLAR', 1100, 'c0d6494d-779f-44cf-8eda-744778b87e3b'),
-      ('5-7 ETAJLAR', 1000, 'c0d6494d-779f-44cf-8eda-744778b87e3b'),
-      ('7-9 ETAJLAR', 900, 'c0d6494d-779f-44cf-8eda-744778b87e3b');
+VALUES('1-2 ETAJLAR', 1200, '268dce62-8937-42cc-ab51-46d5bb2b530c'),
+      ('3-5 ETAJLAR', 1100, '268dce62-8937-42cc-ab51-46d5bb2b530c'),
+      ('5-7 ETAJLAR', 1000, '268dce62-8937-42cc-ab51-46d5bb2b530c'),
+      ('7-9 ETAJLAR', 900, '268dce62-8937-42cc-ab51-46d5bb2b530c');
 
-INSERT INTO golden_house_sagbon(sagbon_house_name, sagbon_house_price, sagbon_of) 
-VALUES('1-2 ETAJLAR', 1000, 'e9bc8b92-a076-4fe5-9209-20b080c69643'),
-      ('3-5 ETAJLAR', 900, 'e9bc8b92-a076-4fe5-9209-20b080c69643'),
-      ('5-7 ETAJLAR', 850, 'e9bc8b92-a076-4fe5-9209-20b080c69643'),
-      ('7-9 ETAJLAR', 800, 'e9bc8b92-a076-4fe5-9209-20b080c69643');
+INSERT INTO sagbon(house_name, house_price, company_id) 
+VALUES('1-2 ETAJLAR', 1000, 'e3a3bde8-cf7e-4b19-aea2-2828f87efc37'),
+      ('3-5 ETAJLAR', 900, 'e3a3bde8-cf7e-4b19-aea2-2828f87efc37'),
+      ('5-7 ETAJLAR', 850, 'e3a3bde8-cf7e-4b19-aea2-2828f87efc37'),
+      ('7-9 ETAJLAR', 800, 'e3a3bde8-cf7e-4b19-aea2-2828f87efc37');
 
-INSERT INTO  golden_house_xosiyat(xosiyat_house_name, xosiyat_house_price, xosiyat_of) 
-VALUES('1-2 ETAJLAR', 900, 'e9bc8b92-a076-4fe5-9209-20b080c69643'),
-      ('3-5 ETAJLAR', 800, 'e9bc8b92-a076-4fe5-9209-20b080c69643'),
-      ('5-7 ETAJLAR', 750, 'e9bc8b92-a076-4fe5-9209-20b080c69643'),
-      ('7-9 ETAJLAR', 650, 'e9bc8b92-a076-4fe5-9209-20b080c69643');
+INSERT INTO  golden_house_xosiyat(house_name, house_price, company_id) 
+VALUES('1-2 ETAJLAR', 900, 'e0e98305-e646-439e-8a1c-492be0dc2f9a'),
+      ('3-5 ETAJLAR', 800, 'e0e98305-e646-439e-8a1c-492be0dc2f9a'),
+      ('5-7 ETAJLAR', 750, 'e0e98305-e646-439e-8a1c-492be0dc2f9a'),
+      ('7-9 ETAJLAR', 650, 'e0e98305-e646-439e-8a1c-492be0dc2f9a');
 
-INSERT INTO  akay_city_mirzo_ulugbek(mirzo_ulugbek_name, mirzo_ulugbek_price, mirzo_ulugbek_of) 
-VALUES('1-2 ETAJLAR', 900, '392f858a-6b00-4092-a291-bbe447a8c881'),
-      ('3-5 ETAJLAR', 800, '392f858a-6b00-4092-a291-bbe447a8c881'),
-      ('5-7 ETAJLAR', 750, '392f858a-6b00-4092-a291-bbe447a8c881'),
-      ('7-9 ETAJLAR', 650, '392f858a-6b00-4092-a291-bbe447a8c881'),
-      ('10-12 ETAJLAR', 630, '392f858a-6b00-4092-a291-bbe447a8c881'),
-      ('13-15 ETAJLAR', 600, '392f858a-6b00-4092-a291-bbe447a8c881');
+INSERT INTO  akay_city_mirzo_ulugbek(mirzo_ulugbek_name, mirzo_ulugbek_price, company_id)
+VALUES('1-2 ETAJLAR', 900, '405f284e-860f-4eb1-92dd-22338509d8eb'),
+      ('3-5 ETAJLAR', 800, '405f284e-860f-4eb1-92dd-22338509d8eb'),
+      ('5-7 ETAJLAR', 750, '405f284e-860f-4eb1-92dd-22338509d8eb'),
+      ('7-9 ETAJLAR', 650, '405f284e-860f-4eb1-92dd-22338509d8eb'),
+      ('10-12 ETAJLAR', 630, '405f284e-860f-4eb1-92dd-22338509d8eb'),
+      ('13-15 ETAJLAR', 600, '405f284e-860f-4eb1-92dd-22338509d8eb');
 
-INSERT INTO  akay_city_yunusobod(yunusobod_name, yunusobod_price, yunusobod_of) 
-VALUES('1-2 ETAJLAR', 1000, '392f858a-6b00-4092-a291-bbe447a8c881'),
-      ('3-5 ETAJLAR', 900, '392f858a-6b00-4092-a291-bbe447a8c881'),
-      ('5-7 ETAJLAR', 850, '392f858a-6b00-4092-a291-bbe447a8c881'),
-      ('7-9 ETAJLAR', 750, '392f858a-6b00-4092-a291-bbe447a8c881'),
-      ('10-12 ETAJLAR', 700, '392f858a-6b00-4092-a291-bbe447a8c881'),
-      ('13-15 ETAJLAR', 650, '392f858a-6b00-4092-a291-bbe447a8c881');
+INSERT INTO  akay_city_yunusobod(yunusobod_name, yunusobod_price, company_id) 
+VALUES('1-2 ETAJLAR', 1000, 'd73af5b2-ab82-49b2-8780-bbc66804517b'),
+      ('3-5 ETAJLAR', 900, 'd73af5b2-ab82-49b2-8780-bbc66804517b'),
+      ('5-7 ETAJLAR', 850, 'd73af5b2-ab82-49b2-8780-bbc66804517b'),
+      ('7-9 ETAJLAR', 750, 'd73af5b2-ab82-49b2-8780-bbc66804517b'),
+      ('10-12 ETAJLAR', 700, 'd73af5b2-ab82-49b2-8780-bbc66804517b'),
+      ('13-15 ETAJLAR', 650, 'd73af5b2-ab82-49b2-8780-bbc66804517b');
 
 
 
--- src/model/model.sql
+++ src/model/model.sql
@@ -48,6 +48,28 @@ CREATE TABLE golden_house (
   ON DELETE CASCADE
 );
 
+CREATE TABLE sagbon (
+  house_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  house_name varchar(50) NOT NULL,
+  house_price decimal(10, 2),
+  house_started_at timestampTz DEFAULT CURRENT_TIMESTAMP,
+  company_id uuid NOT NULL,
+    FOREIGN KEY (company_id)
+    REFERENCES golden_house(building_id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE golden_house_xosiyat (
+  house_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  house_name varchar(50) NOT NULL,
+  house_price decimal(10, 2),
+  house_started_at timestampTz DEFAULT CURRENT_TIMESTAMP,
+  company_id uuid NOT NULL,
+    FOREIGN KEY (company_id)
+    REFERENCES golden_house(building_id)
+    ON DELETE CASCADE
+);
+
 CREATE TABLE akay_city (
   building_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
   building_name varchar(50) NOT NULL,
@@ -58,6 +80,27 @@ CREATE TABLE akay_city (
   ON DELETE CASCADE
 );
 
+CREATE TABLE akay_city_mirzo_ulugbek (
+  mirzo_ulugbek_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  mirzo_ulugbek_name varchar(50) NOT NULL,
+  mirzo_ulugbek_price decimal(10, 2) NOT NULL,
+  mirzo_ulugbek_started_at timestampTz DEFAULT CURRENT_TIMESTAMP,
+  company_id uuid NOT NULL,
+    FOREIGN KEY (company_id)
+    REFERENCES akay_city(building_id)
+    ON DELETE CASCADE
+);
+
+CREATE TABLE akay_city_yunusobod (
+  yunusobod_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+  yunusobod_name varchar(50) NOT NULL,
+  yunusobod_price decimal(10, 2) NOT NULL,
+  yunusobod_started_at timestampTz DEFAULT CURRENT_TIMESTAMP,
+  company_id uuid NOT NULL,
+    FOREIGN KEY (company_id)
+    REFERENCES akay_city(building_id)
+    ON DELETE CASCADE
+);
 
 SELECT 
   *
@@ -67,48 +110,6 @@ RIGHT JOIN
   murad_buildings
 USING (company_id);
 
-CREATE TABLE sagbon (
-  sagbon_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-  sagbon_house_name varchar(50) NOT NULL,
-  sagbon_house_price decimal(10, 2),
-  golden_house_started_at timestampTz DEFAULT CURRENT_TIMESTAMP,
-  company_id uuid NOT NULL,
-    FOREIGN KEY (sagbon_of)
-    REFERENCES building_companies(building_company_id)
-    ON DELETE CASCADE
-);
 
-CREATE TABLE golden_house_xosiyat (
-  xosiyat_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-  xosiyat_house_name varchar(50) NOT NULL,
-  xosiyat_house_price decimal(10, 2),
-  xosiyat_house_started_at timestampTz DEFAULT CURRENT_TIMESTAMP,
-  xosiyat_of uuid,
-    FOREIGN KEY (xosiyat_of)
-    REFERENCES building_companies(building_company_id)
-    ON DELETE CASCADE
-);
-
-CREATE TABLE akay_city_mirzo_ulugbek (
-  mirzo_ulugbek_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-  mirzo_ulugbek_name varchar(50) NOT NULL,
-  mirzo_ulugbek_price decimal(10, 2) NOT NULL,
-  mirzo_ulugbek_started_at timestampTz DEFAULT CURRENT_TIMESTAMP,
-  mirzo_ulugbek_of uuid,
-    FOREIGN KEY (mirzo_ulugbek_of)
-    REFERENCES building_companies(building_company_id)
-    ON DELETE CASCADE
-);
-
-CREATE TABLE akay_city_yunusobod (
-  yunusobod_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-  yunusobod_name varchar(50) NOT NULL,
-  yunusobod_price decimal(10, 2) NOT NULL,
-  yunusobod_started_at timestampTz DEFAULT CURRENT_TIMESTAMP,
-  yunusobod_of uuid,
-    FOREIGN KEY (yunusobod_of)
-    REFERENCES building_companies(building_company_id)
-    ON DELETE CASCADE
-);
 
 
